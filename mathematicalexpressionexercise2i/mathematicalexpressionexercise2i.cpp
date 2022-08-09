b600a137b87d72c9ba00b707d0b5557cd0ee7480
mbashit <michaelbashitapwa52@gmail.com>
2022-08-09 22:50:14 +0300

    Add project files.

---
+++ mathematicalexpressionexercise2i/mathematicalexpressionexercise2i.cpp
@@ -0,0 +1,22 @@
+// mathematicalexpressionexercise2i.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	double x, w, y;
+
+	cout << "enter the value of x";
+	cin >> x;
+	cout << "enter the value of w ";
+	cin >> w;
+
+	y = (x + 3) * pow(5, w) / 7 * (x - 4);
+	cout << "the result is" << y << endl;
+
+	return 0;
+}
+
